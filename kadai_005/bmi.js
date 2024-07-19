901b817bebbf8575c92cb5f28b82e392f4e276d7
Akiho Tabata <ackee.gamer1998@gmail.com>
2024-07-19 10:24:26 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -3,4 +3,6 @@ let bodyWeight = 68;
 //身長
 let bodyHeight =1.7;
 //BMI計算式
-console.log(bodyWeight/(bodyHeight*bodyHeight));
+let bmiResult = (bodyWeight/(bodyHeight*bodyHeight));
+//BMIの計算結果
+console.log(bmiResult);
